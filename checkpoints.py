99498ac0d82cc428a9cb2bbdf8f339e408f12d65
Asa Paparo <asapaparo@gmail.com>
2023-07-17 09:25:06 -0400

    surely

--- checkpoints.py
+++ checkpoints.py
@@ -39,7 +39,7 @@ class Solution:
             #return type: int
 
             for i in range(len(checkpoints)):
-                for j in range(len(checkpoints) - 1):
+                for j in range(i, len(checkpoints)):
                     if checkpoints[j] < checkpoints[i]:
                         tmp = checkpoints[i]
                         checkpoints[i] = checkpoints[j]
